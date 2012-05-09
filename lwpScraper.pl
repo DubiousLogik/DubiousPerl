0c638da3e471e755e8320299db08d1e1498a2524
Robbie Devine <devinerob@hotmail.com>
2012-05-08 22:14:08 -0700

    first commit for 2 new files

---
+++ lwpScraper.pl
@@ -0,0 +1,132 @@
+# --------------------------------------------------------------------------------------------
+#!/usr/bin/perl
+# lwpScraper.pl
+# robbie devine 
+# 03 May 2012
+# --------------------------------------------------------------------------------------------
+# counts bytes of click urls vs overall page weight for common search engine pages
+# Notes:  this is written quick and dirty, data is piped thru files
+# due to easy use of regex substitution and selective introduction of 
+# line breaks where I want them.  This could be made more efficient by 
+# using split and arrays to keep all the work in memory.  OTOH since 
+# you are making web call, you'll pay ~1 sec per query anyway, the 
+# extra few ms for disk I/O may be trivial overall.
+# --------------------------------------------------------------------------------------------
+
+#package EdgarBot;
+#use strict;
+use LWP; # 5.64;
+
+print "Starting lwpScraper.pl ----------------------------------------------\n";
+
+# constants
+my $file1 = "scrape1.txt";
+my $queryFile = "queries.txt";
+my $baseUrl = "http://bing.com/search?q=";
+my $query = "";
+
+my $browser = LWP::UserAgent->new;
+#Use this only if a proxy exists
+#$browser->proxy(['http', 'ftp'], 'http://domain.com:8080/');
+
+open(QUERIES, "<$queryFile");
+
+while (<QUERIES>) {
+	$query = $_;
+	$query =~ s/\n//;  #remove line breaks
+	# print "Processing query: [" . $query . "]\n"; # debugging
+	open(FILE1, ">$file1");
+	my $response = $browser->get( $baseUrl.$query );
+		die "Can't get $url -- ", $response->status_line
+			unless $response->is_success;
+		die "Site not found", $response->content_type
+			unless $response->content_type eq 'text/html';
+
+		print FILE1 $response->content;
+		close(FILE1);
+		urlWeight($file1,$query);
+}
+
+print "\n---------------------------------------------- Exiting lwpScraper.pl\n";
+
+sub urlWeight {
+
+	my $filename = $_[0];
+	my $query = $_[1];
+	
+	my $outputText = "";
+	my $totalCharCount = 0;  
+	my $urlCharCount = 0;
+	my $lineCount = 0;
+	my $urlCount = 0;
+	my $pattern = "r\.msn\.com"; #use for bing
+	#my $pattern = "google\.com\/aclk"; #use for Google
+
+	open(TEMP, ">temp.txt");
+	open(INPUT, "<$filename");
+	while(<INPUT>) {
+		if ($_) {
+			$outputText = $_;
+			$outputText =~ s/\n//g;    #remove line breaks, create 1 long line
+			$lineCount++;
+			print TEMP $outputText;
+		}
+	}
+
+	close(INPUT);
+	close(TEMP);
+	#print "lineCount1 = " . $lineCount . "\n"; # debugging
+
+	$outputText = "";
+	$lineCount = 0;
+
+	#use temp file as input to next stage in pipeline
+	my $tabs = 0;
+	open(TEMP2, ">temp2.txt");  
+	open(TEMP, "<temp.txt");
+	while(<TEMP>) {
+		if ($_) {
+			$outputText = $_;		
+			$totalCharCount = length($outputText);
+			$outputText =~ s/\>/\>\n/g; #add line breaks after closing tags, ensure 1 element per line
+			print TEMP2 $outputText;
+			$lineCount++;
+		}
+	}
+
+	close(TEMP);
+	close(TEMP2);
+	#print "lineCount2 = " . $lineCount . "\n"; #debugging
+
+	$outputText = "";
+	$lineCount = 0;
+
+	open(TEMP2, "<temp2.txt");
+	while(<TEMP2>) {
+		#look for Urls here
+		if ($_) {
+			$outputText = $_;		
+			if ($outputText =~ m/$pattern/) {
+				$outputText =~ s/\<a href\=\"(\S+)\" h=(\S+)\"\>/$1/; #remove extra stuff around the url itself #unique to bing, leave it for now
+				#print $outputText;  # debugging
+				$urlCharCount += length($outputText);
+				$urlCount++;
+			}
+			$lineCount++;
+		}	
+	}
+	close(TEMP2);
+
+	#print "lineCount3 = " . $lineCount . "\n"; # debugging
+	print "\n------------------------------\n";
+	print "Query: [" . $query . "]\n";
+	print "Total Char Count: " . $totalCharCount . "\n";
+	print "Total Url Count: " . $urlCount . "\n";
+	print "Total Url Char Count: " . $urlCharCount . "\n";
+	if ($totalCharCount>0) {
+		print "Url Count Weight (Url Chars / Page Chars) = " . ($urlCharCount/$totalCharCount) . "\n";
+	}
+	#print "\n------------------------------\n";
+
+
+}
---
+++ parseWebCrawlText.pl
@@ -0,0 +1,82 @@
+#--------------------------------------------------------------------
+#  parseWebCrawlText.pl
+#  Script to parse web crawl results
+#
+#  Author:       robbie devine  
+#--------------------------------------------------------------------
+
+use encoding 'utf8';
+
+$i = 0; # hits counter
+$j = 0; # line counter
+$line = "";
+$debugLimit = 10000000;  #limit rows processed for debugging
+
+my $source = "C:\\Users\\Stage 2.1\\output more urls.txt";
+my $target = "C:\\Users\\Stage 2.1\\Filtered Output more Url sample.txt";
+
+$t0 = time();
+print "\nProcessing...\n";
+
+open(TRG, ">$target");
+open(SRC, "$source");
+
+while (defined ($line = <SRC>) && ($j < $debugLimit)) {
+
+	$FullUrl = $TopLevelDomain = $MetaDescription = $Keywords = $Title = "";
+	$LastUrl = $kwFound = $metaFound = $titleFound = 0;
+
+	chomp $line;
+	$j++; 
+	@words = split /\|/,$line;
+	#print "$line\n";
+	for ($n=0; $n<scalar(@words); $n++) {
+		#print "@words[$n]\n";
+		if (@words[$n] =~ m/^http[s]?:\/\//i) {
+			$LastUrl++;
+		}
+		if (@words[$n] =~ m/<[\s]*meta[\s]*name[\s]*[=][\s]*[\"|\']description[\"|\'][\s]*content[\s]*[=][\s]*[\"|\'](.*)[\"|\']/i) {
+			if ($1 !~ m/\?{3,}/) {
+				$MetaDescription = $1;
+				$metaFound = 1;
+			}
+		}
+		if (@words[$n] =~ m/<[\s]*meta[\s]*name[\s]*[=][\s]*[\"|\']keywords[\"|\'][\s]*content[\s]*[=][\s]*[\"|\'](.*)[\"|\']/i) {
+			if ($1 !~ m/\?{3,}/) {
+				$Keywords = $1;
+				$kwFound = 1;
+			}
+		}
+		if (@words[$n] =~ m/<[\s]*title[\s]*>(.*)</i) {
+			if ($1 !~ m/\?{3,}/) {
+				$Title = $1;
+				$titleFound = 1;
+			}
+		}
+	}
+	$FullUrl = @words[$LastUrl-1];
+	#if ($kwFound || $metaFound || $titleFound) { $i++; }	
+	if ($metaFound || $titleFound) { $i++; }	
+	
+	print TRG "$FullUrl\t";
+	print TRG "$Title\t";
+	print TRG "$MetaDescription\t";
+	print TRG "$Keywords\n";
+}
+
+close(SRC);
+close(TRG);
+
+$t1 = time();  
+$r1 = ($t1 - $t0)/60;
+
+print "Runtime = $r1 minutes\n";
+print "Total Rows: $j\nTotal Hits: $i\n";
+print "\nEnding Run...";
+
+
+#	if  ( $line =~ m/(([\w-]*)(\.))?([\w-]*)(\.)(com|net|org)/ ) {
+#		$i++;
+#		if (length($2) > 0) { print TRG "$2$3$4$5$6\n"; }
+#		if (length($4) > 0) { print TRG "$4$5$6\n"; }
+#	} 
