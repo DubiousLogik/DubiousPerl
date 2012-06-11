fe08391d56ff3aeb956264c3e6daa5f69addc6e6
Robbie Devine <devinerob@hotmail.com>
2012-06-11 11:46:08 -0700

    updating to check for numbers (duh)

--- passwordStrength.pl
+++ passwordStrength.pl
@@ -3,7 +3,7 @@
 # 11 June 2012
 # robbie devine
 
-my $programName = "passwordStrength.pl v2.2";
+my $programName = "passwordStrength.pl v2.3";
 
 print "\nStarting $programName --------------------------------------------\n\n";
 my $password = $ARGV[0];  #input param
@@ -14,6 +14,7 @@ print "Processing password candidate $password\n\n";
 if (length($password) < 8 ) { print "password must be at least 8 characters\n"; $ok=0; }
 if ($password !~ m/[A-Z]/)  { print "password must contain upper case letters\n"; $ok=0; }
 if ($password !~ m/[a-z]/)  { print "password must contain lower case letters\n"; $ok=0; }
+if ($password !~ m/[0-9]/)  { print "password must contain numbers\n"; $ok=0; }
 if ($password !~ m/[!@#\$%^&*()_\-\[\]\'\"]/) { print "password must contain special characters\n"; $ok=0; }
 if ($password =~ /(\S)(?=\1{2})/) { print "password cannot contain 3 or more identical characters in a row\n"; $ok=0; }
 
